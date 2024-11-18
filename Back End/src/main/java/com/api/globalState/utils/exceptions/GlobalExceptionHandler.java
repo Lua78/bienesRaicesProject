71d72da4986d3a694cf31a18ae7040c6d0041130
Luis Urbiba <urbinaismael56@gmail.com>
2024-11-18 17:27:52 -0600

    feat:Backend: added DataIntegrityViolationException to exception handler

--- Back End/src/main/java/com/api/globalState/utils/exceptions/GlobalExceptionHandler.java
+++ Back End/src/main/java/com/api/globalState/utils/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.api.globalState.utils.exceptions;
 
 import com.api.globalState.utils.GenericResponse;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,5 +12,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = ResponseException.class)
     protected ResponseEntity<GenericResponse> handleResponseException(ResponseException ex) {
         return ResponseEntity.badRequest().body(new GenericResponse<>(ex.getMessage()));
+    }
+
+    @ExceptionHandler(value = DataIntegrityViolationException.class)
+    protected ResponseEntity<GenericResponse> handleIntegrityException(ResponseException ex) {
+        return ResponseEntity.badRequest().body(new GenericResponse<>("A database constraint was violated: " + ex.getMessage()));
     }
 }
